cd17175aef7e711e39b2aa66593956d787e8adcb
hussainali99a <117758982+hussainali99a@users.noreply.github.com>
2024-09-10 15:18:22 +0530

    Add files via upload

--- day1.py
+++ day1.py
@@ -2,23 +2,34 @@ from flask import Flask , redirect , url_for
 
 app = Flask(__name__)
 
-@app.route("/success/<int:score>")
-def success(score):
-    return "<h1>My 12th Score is </h1>"+str(score) +"<h1>pass</h1>"
+@app.route("/range1/<int:num>")
+def range1(num):
+    return "<h1>range is </h1>"+str(num) +"<h1>btw 101 to 201</h1>"
 
-@app.route("/fail/<int:score>")
-def fail(score):
-    return "<h1>My 12th Score is </h1>"+str(score)+"<h1>fail</h1>"
+@app.route("/range2/<int:num>")
+def range2(num):
+    return "<h1>range is  </h1>"+str(num)+"<h1>btw 201 to 301</h1>"
 
-@app.route("/result/<int:marks>")
-def result(marks):
+@app.route("/range3/<int:num>")
+def range3(num):
+    return "<h1>range is  </h1>"+str(num)+"<h1>btw 301 to 401</h1>"
+
+@app.route("/norange/<int:num>")
+def norange(num):
+    return "<h1>range is  </h1>"+str(num)+"<h1>no range </h1>"
+@app.route("/result/<int:no>")
+def result(no):
     result = ""
-    if marks > 50:
-        result = "success"
+    if no > 101 and no<201:
+        result = "range1"
+    elif no > 201 and no<301:
+        result = "range2"
+    elif no > 301 and no<401:
+        result = "range3"
     else:
-        result = "fail"
+        result = "norange"
         
-    return redirect(url_for(result ,  score=marks))
+    return redirect(url_for(result ,  num=no))
 
 
 if __name__ == "__main__":
